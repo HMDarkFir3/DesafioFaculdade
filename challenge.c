2dae1e56353c71edc334cda060d2dd44cbe1769e
Henrique Luís Oliveira Marques <henriquestudo@outlook.com>
2021-03-30 00:25:27 -0300

    Configurando funções

--- challenge.c
+++ challenge.c
@@ -1,27 +1,264 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
-#include <unistd.h>
 
 //Utils ----------------------------------------------------|
-void clearScreen() {
+void clearScreen() { // Limpa tela
     system("cls");
 }
 
-void jumpLine() {
+void jumpLine() { // Pula linha
     printf("\n");
 }
 
 //Menu Options --------------------------------------------|
-//7 --|--
-void quit(int change) {
-    int id = 0;
+//1 --|--
+void dataEntries() {
+    int change;
 
+    //Limpa tela
+    clearScreen();
+
+    //Linha superior
+    buildMenuTopLine();
+
+    //Menu item
+    buildMenuItem(" 1 - Voltar");
+    jumpLine(); // Pula linha
+    buildMenuItem(" 2 - Escrever no arquivo");
+    jumpLine(); // Pula linha
+
+    //Linha inferior
+    buildMenuBottomLine();
+
+    printf("\tEscolha uma das opcoes acima: ");
+    scanf("%d", &change);
+
+    switch(change) {
+        case 1:
+            menu();
+            break;
+        case 2:
+            printf("\tEntrada de dados");
+            break;
+        default:
+            clearScreen();
+            printf("\tErro no sistema.");
+            Sleep(2000);
+            dataEntries();
+            break;
+    }
+
+}
+
+//2 --|--
+void dataList() {
+    int change;
+
+    //Limpa tela
+    clearScreen();
+
+    //Linha superior
+    buildMenuTopLine();
+
+    //Menu item
+    buildMenuItem(" 1 - Voltar");
+    jumpLine(); // Pula linha
+    buildMenuItem(" 2 - Listar dados");
+    jumpLine(); // Pula linha
+
+    //Linha inferior
+    buildMenuBottomLine();
+
+    printf("\tEscolha uma das opcoes acima: ");
+    scanf("%d", &change);
+
+    switch(change) {
+        case 1:
+            menu();
+            break;
+        case 2:
+            printf("\tListar de dados");
+            break;
+        default:
+            clearScreen();
+            printf("\tErro no sistema.");
+            Sleep(2000);
+            dataList();
+            break;
+    }
+}
+
+//3 --|--
+void dataSearchName() {
+    int change;
+
+    //Limpa tela
+    clearScreen();
+
+    //Linha superior
+    buildMenuTopLine();
+
+    //Menu item
+    buildMenuItem(" 1 - Voltar");
+    jumpLine(); // Pula linha
+    buildMenuItem(" 2 - Pesquisar dados por nome");
+    jumpLine(); // Pula linha
+
+    //Linha inferior
+    buildMenuBottomLine();
+
+    printf("\tEscolha uma das opcoes acima: ");
+    scanf("%d", &change);
+
+    switch(change) {
+        case 1:
+            menu();
+            break;
+        case 2:
+            printf("\tPesquisa de dados por nome");
+            break;
+        default:
+            clearScreen();
+            printf("\tErro no sistema.");
+            Sleep(2000);
+            dataSearchName();
+            break;
+    }
+}
+
+//4 --|--
+void dataSearchLetter() {
+    int change;
+
+    //Limpa tela
+    clearScreen();
+
+    //Linha superior
+    buildMenuTopLine();
+
+    //Menu item
+    buildMenuItem(" 1 - Voltar");
+    jumpLine(); // Pula linha
+    buildMenuItem(" 2 - Pesquisar dados por letra");
+    jumpLine(); // Pula linha
+
+    //Linha inferior
+    buildMenuBottomLine();
+
+    printf("\tEscolha uma das opcoes acima: ");
+    scanf("%d", &change);
+
+    switch(change) {
+        case 1:
+            menu();
+            break;
+        case 2:
+            printf("\tPesquisa de dados por letra");
+            break;
+        default:
+            clearScreen();
+            printf("\tErro no sistema.");
+            Sleep(2000);
+            dataSearchLetter();
+            break;
+    }
+}
+
+//5 --|--
+void dataEdit() {
+    int change;
+
+    //Limpa tela
+    clearScreen();
+
+    //Linha superior
+    buildMenuTopLine();
+
+    //Menu item
+    buildMenuItem(" 1 - Voltar");
+    jumpLine(); // Pula linha
+    buildMenuItem(" 2 - Alterar dados");
+    jumpLine(); // Pula linha
+
+    //Linha inferior
+    buildMenuBottomLine();
+
+    printf("\tEscolha uma das opcoes acima: ");
+    scanf("%d", &change);
+
+    switch(change) {
+        case 1:
+            menu();
+            break;
+        case 2:
+            printf("\Alterar dados");
+            break;
+        default:
+            clearScreen();
+            printf("\tErro no sistema.");
+            Sleep(2000);
+            dataEdit();
+            break;
+    }
+}
+
+//6 --|--
+void dataDelete() {
+    int change;
+
+    //Limpa tela
+    clearScreen();
+
+    //Linha superior
+    buildMenuTopLine();
+
+    //Menu item
+    buildMenuItem(" 1 - Voltar");
+    jumpLine(); // Pula linha
+    buildMenuItem(" 2 - Excluir dados");
+    jumpLine(); // Pula linha
+
+    //Linha inferior
+    buildMenuBottomLine();
+
+    printf("\tEscolha uma das opcoes acima: ");
+    scanf("%d", &change);
+
+    switch(change) {
+        case 1:
+            menu();
+            break;
+        case 2:
+            printf("\Excluir dados");
+            break;
+        default:
+            clearScreen();
+            printf("\tErro no sistema.");
+            Sleep(2000);
+            dataDelete();
+            break;
+    }
+}
+
+//7 --|--
+void quit() { // Fechar o programa
     printf("\tFinalizando o programa...");
     Sleep(3000);
 }
 
 //Build Menu -----------------------------------------------|
+void buildMenuTopLine() {
+    //Menu Top --------------------------------------------|
+    printf("\t%c", 201);
+
+    for(int i = 0; i < 36; i++) {
+        printf("%c", 205);
+    }
+
+    printf("%c", 187);
+
+    jumpLine();
+}
+
 void buildMenuItem(char str[]) {
     int countLetters = 0;
 
@@ -44,37 +281,7 @@ void buildMenuItem(char str[]) {
     printf("%c", 186);
 }
 
-void menu() {
-    //Variables
-    int change;
-
-    //Menu Top --------------------------------------------|
-    printf("\t%c", 201);
-
-    for(int i = 0; i < 36; i++) {
-        printf("%c", 205);
-    }
-
-    printf("%c", 187);
-
-    jumpLine();
-
-    //Menu Item -------------------------------------------|
-    buildMenuItem(" 1 - Entrada de dados");
-    jumpLine(); //
-    buildMenuItem(" 2 - Listar dados");
-    jumpLine(); //
-    buildMenuItem(" 3 - Pesquisar produto");
-    jumpLine(); //
-    buildMenuItem(" 4 - Pesquisar produto pela 1 letra");
-    jumpLine(); //
-    buildMenuItem(" 5 - Alterar dados");
-    jumpLine(); //
-    buildMenuItem(" 6 - Excluir dados");
-    jumpLine(); //
-    buildMenuItem(" 7 - Sair");
-    jumpLine(); //
-
+void buildMenuBottomLine() {
     //Menu Bottom -----------------------------------------|
     printf("\t%c", 200);
 
@@ -83,32 +290,69 @@ void menu() {
     }
 
     printf("%c", 188);
+
     jumpLine(); //
+}
+
+void menu() {
+    //Variavel
+    int change;
+
+    //Limpa tela
+    clearScreen();
+
+    //Linha superior
+    buildMenuTopLine();
+
+    //Menu Item -------------------------------------------|
+    buildMenuItem(" 1 - Entrada de dados");
+    jumpLine(); // Pula linha
+    buildMenuItem(" 2 - Listar dados");
+    jumpLine(); // Pula linha
+    buildMenuItem(" 3 - Pesquisar dados por nome");
+    jumpLine(); // Pula linha
+    buildMenuItem(" 4 - Pesquisar dados pela 1 letra");
+    jumpLine(); // Pula linha
+    buildMenuItem(" 5 - Alterar dados");
+    jumpLine(); // Pula linha
+    buildMenuItem(" 6 - Excluir dados");
+    jumpLine(); // Pula linha
+    buildMenuItem(" 7 - Sair");
+    jumpLine(); // Pula linha
+
+    //Linha inferior
+    buildMenuBottomLine();
 
     printf("\tEscolha uma das opcoes acima: ");
     scanf("%d", &change);
 
     switch(change) {
         case 1:
-            printf("\tErro no sistema!");
+            dataEntries();
             break;
         case 2:
-            printf("\tErro no sistema!");
+            dataList();
             break;
         case 3:
-            printf("\tErro no sistema!");
+            dataSearchName();
             break;
         case 4:
-            printf("\tErro no sistema!");
+            dataSearchLetter();
             break;
         case 5:
-            printf("\tErro no sistema!");
+            dataEdit();
             break;
         case 6:
-            printf("\tErro no sistema!");
+            dataDelete();
             break;
         case 7:
-            quit(change);
+            quit();
+            break;
+        default:
+            clearScreen();
+            printf("\tErro no sistema.");
+            Sleep(2000);
+            menu();
             break;
     }
 }
