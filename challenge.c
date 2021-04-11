01dcd8895657d373d3cae53498485554e3763af2
Henrique Luís Oliveira Marques <henriquestudo@outlook.com>
2021-04-10 23:26:12 -0300

    Completo

--- challenge.c
+++ challenge.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
-#include <ctype.h>
-#include <string.h>
 
 #define RED   "\x1B[31m"
 #define GRN   "\x1B[32m"
@@ -19,28 +17,7 @@ int amount1 = 0;
 int amount2 = 0;
 int amount3 = 0;
 
-char validation[52] = {
-    'a', 'b', 'c',
-    'd', 'e', 'f',
-    'g', 'h', 'i',
-    'j', 'k', 'l',
-    'm', 'n', 'o',
-    'p', 'q', 'r',
-    's', 't', 'u',
-    'v', 'w', 'x',
-    'y', 'z',
-    'A', 'B', 'C',
-    'D', 'E', 'F',
-    'G', 'H', 'I',
-    'J', 'K', 'L',
-    'M', 'N', 'O',
-    'P', 'Q', 'R',
-    'S', 'T', 'U',
-    'V', 'W', 'X',
-    'Y', 'Z',
-};
-
-//Utils ------------------------------------------------------|X*
+/// Utils ------------------------------------------------------|X*
 void clearScreen() { // Limpa tela
     system("cls");
 }
@@ -57,7 +34,7 @@ int searchName() {
     char product[20];
     int i, j;
 
-    printf("\tProcurar produro pelo nome: ");
+    printf("\tProcurar produdo pelo nome: ");
     scanf("%s", &product);
 
     f = fopen("product.txt", "r");
@@ -88,26 +65,26 @@ int searchName() {
     fclose(f);
     return -1;
 }
-// -----------------------------------------------------------|X*
+/// -----------------------------------------------------------|X*
 
-// Menu Options ----------------------------------------------|O*
-// 1 --|--
-void dataEntries() { // Entrada de dados
+/// Menu Options ----------------------------------------------|O*
+/// 1 --|--
+void dataEntries() { /// Entrada de dados
     int change;
 
-    // Limpa tela
+    /// Limpa tela
     clearScreen();
 
-    // Linha superior
+    /// Linha superior
     buildMenuTopLine();
 
-    // Menu item
+    /// Menu item
     buildMenuItem(" 1 - Voltar");
     jumpLine(); // Pula linha
     buildMenuItem(" 2 - Escrever no arquivo");
     jumpLine(); // Pula linha
 
-    // Linha inferior
+    /// Linha inferior
     buildMenuBottomLine();
 
     printf("\tEscolha uma das opcoes acima: ");
@@ -115,6 +92,7 @@ void dataEntries() { // Entrada de dados
 
     switch(change) {
         case 1:
+            clearScreen();
             menu();
             break;
         case 2:
@@ -125,32 +103,13 @@ void dataEntries() { // Entrada de dados
             f = fopen("product.txt", "w");
 
             int i, j;
-            int validate;
 
-            // Validação produto 1
+            printf("\tDigite o 1 produto: ");
+            scanf("%s", &product1);
+
+            /// Validação quatidade 1
             do {
-                validate = 0;
 
-                printf("\tDigite o 1 produto: ");
-                scanf("%s", &product1);
-
-                for(i = 0; i < strlen(product1); i++) {
-                    for(j = 0; j < strlen(validation); j++) {
-                        if(product1[i] == validation[j]) {
-                            validate++;
-                        }
-                    }
-                }
-
-                if(validate != strlen(product1)) {
-                    printf(YEL "\tATENCAO: Nao pode digitar numeros e/ou caracteres especiais.\n" RESET);
-                    jumpLine();
-                }
-
-            } while(validate != strlen(product1));
-
-            // Validação quatidade 1
-            do {
                 printf("\tDigite a quantidade de %s: ", product1);
                 scanf("%d", &amount1);
 
@@ -161,31 +120,12 @@ void dataEntries() { // Entrada de dados
 
             } while(amount1 < 0 || amount1 == 10);
 
-            jumpLine(); // Pula linha
+            jumpLine(); /// Pula linha
 
-            // Validação produto 2
-            do {
-                validate = 0;
+            printf("\tDigite o 2 produto: ");
+            scanf("%s", &product2);
 
-                printf("\tDigite o 2 produto: ");
-                scanf("%s", &product2);
-
-                for(i = 0; i < strlen(product2); i++) {
-                    for(j = 0; j < strlen(validation); j++) {
-                        if(product2[i] == validation[j]) {
-                            validate++;
-                        }
-                    }
-                }
-
-                if(validate != strlen(product2)) {
-                    printf(YEL "\tATENCAO: Nao pode digitar numeros e/ou caracteres especiais.\n" RESET);
-                    jumpLine();
-                }
-
-            } while(validate != strlen(product2));
-
-            // Validação quatidade 2
+            /// Validação quatidade 2
             do {
                 printf("\tDigite a quantidade de %s: ", product2);
                 scanf("%d", &amount2);
@@ -197,27 +137,12 @@ void dataEntries() { // Entrada de dados
 
             } while(amount2 < 0 || amount2 == 10);
 
-            jumpLine(); // Pula linha
+            jumpLine(); /// Pula linha
 
-            // Validação produto 3
-            do {
-                printf("\tDigite o 3 produto: ");
-                scanf("%s", &product3);
+            printf("\tDigite o 3 produto: ");
+            scanf("%s", &product3);
 
-                for(i = 0; i < strlen(product3); i++) {
-                    for(j = 0; j < strlen(validation); j++) {
-                        validate++;
-                    }
-                }
-
-                if(validate != strlen(product3)) {
-                    printf(YEL "\tATENCAO: Nao pode digitar numeros e/ou caracteres especiais.\n" RESET);
-                    jumpLine();
-                }
-
-            } while(validate != strlen(product3));
-
-            // Validação quatidade 3
+            /// Validação quatidade 3
             do {
                 printf("\tDigite a quantidade de %s: ", product3);
                 scanf("%d", &amount3);
@@ -229,15 +154,15 @@ void dataEntries() { // Entrada de dados
 
             } while(amount3 < 0 || amount3 == 10);
 
-            // Produto 1
+            /// Produto 1
             fwrite(&product1, sizeof(product1), 1, f);
             fwrite(&amount1, sizeof(amount1), 1, f);
 
-            // Produto 2
+            /// Produto 2
             fwrite(&product2, sizeof(product2), 1, f);
             fwrite(&amount2, sizeof(amount2), 1, f);
 
-            //Produto 3
+            /// Produto 3
             fwrite(&product3, sizeof(product3), 1, f);
             fwrite(&amount3, sizeof(amount3), 1, f);
 
@@ -245,117 +170,93 @@ void dataEntries() { // Entrada de dados
 
             clearScreen();
             printf(GRN "\tProdutos adicionados com sucesso." RESET);
-            Sleep(2500);
+            Sleep(1500);
 
-            dataEntries();
-            break;
-        default:
             clearScreen();
-            printf(RED "\tErro no sistema." RESET);
-            Sleep(2000);
-            clearScreen();
-            dataEntries();
-            break;
-    }
-
-}
-
-// 2 --|--
-void dataList() { // Listar dados
-    int change;
-
-    //Linha superior
-    buildMenuTopLine();
-
-    //Menu item
-    buildMenuItem(" 1 - Voltar");
-    jumpLine(); // Pula linha
-    buildMenuItem(" 2 - Listar dados");
-    jumpLine(); // Pula linha
-
-    //Linha inferior
-    buildMenuBottomLine();
-
-    printf("\tEscolha uma das opcoes acima: ");
-    scanf("%d", &change);
-
-    switch(change) {
-        case 1:
             menu();
             break;
-        case 2:
-            clearScreen();
-
-            FILE *f;
-
-            f = fopen("product.txt", "r");
-
-            if(f == NULL) {
-                clearScreen();
-                printf(YEL "\tATENCAO: Nenhum arquivo encontrado." RESET);
-                Sleep(2500);
-
-                clearScreen();
-                printf(BLU "\tRedirecionando para entrada de dados." RESET);
-                Sleep(2500);
-
-                dataEntries();
-            }
-
-            printf("\tProduto(s):\n");
-
-            fread(&product1, sizeof(product1), 1, f);
-            fread(&amount1, sizeof(amount1), 1, f);
-
-            if(product1 && amount1) {
-                printf("\t%s: %d unidade(s)\n", product1, amount1);
-            }
-
-            fread(&product2, sizeof(product2), 1, f);
-            fread(&amount2, sizeof(amount2), 1, f);
-
-            if(product2 && amount2) {
-                printf("\t%s: %d unidade(s)\n", product2, amount2);
-            }
-
-            fread(&product3, sizeof(product3), 1, f);
-            fread(&amount3, sizeof(amount3), 1, f);
-
-            if(product3 && amount3) {
-                printf("\t%s: %d unidade(s)\n", product3, amount3);
-            }
-
-            jumpLine();
-
-            fclose(f);
-
-            dataList();
-            break;
         default:
             clearScreen();
             printf(RED "\tErro no sistema." RESET);
-            Sleep(2000);
+            Sleep(1500);
             clearScreen();
-            dataList();
+            dataEntries();
             break;
     }
+
 }
 
-// 3 --|--
+/// 2 --|--
+void dataList() { /// Listar dados
+    int change;
+
+    FILE *f;
+
+    f = fopen("product.txt", "r");
+
+    if(f == NULL) {
+        clearScreen();
+        printf(YEL "\tATENCAO: Nenhum arquivo encontrado." RESET);
+        Sleep(1500);
+
+        clearScreen();
+        printf(BLU "\tRedirecionando para entrada de dados." RESET);
+        Sleep(1500);
+
+        dataEntries();
+    }
+
+    printf("\tProduto(s):\n");
+
+    fread(&product1, sizeof(product1), 1, f);
+    fread(&amount1, sizeof(amount1), 1, f);
+
+    if(product1 && amount1) {
+        printf("\t%s: %d unidade(s)\n", product1, amount1);
+    }
+
+    fread(&product2, sizeof(product2), 1, f);
+    fread(&amount2, sizeof(amount2), 1, f);
+
+    if(product2 && amount2) {
+        printf("\t%s: %d unidade(s)\n", product2, amount2);
+    }
+
+    fread(&product3, sizeof(product3), 1, f);
+    fread(&amount3, sizeof(amount3), 1, f);
+
+    if(product3 && amount3) {
+        printf("\t%s: %d unidade(s)\n", product3, amount3);
+    }
+
+    jumpLine();
+
+    fclose(f);
+
+    printf("\tPressione qualquer tecla para continuar...");
+    getch();
+
+    menu();
+}
+
+/// 3 --|--
 void dataSearchName() { // Pesquisar dados por nome
     int change;
     int search;
 
-    //Linha superior
+    /// Limpa tela
+    clearScreen();
+
+    /// Linha superior
     buildMenuTopLine();
 
-    //Menu item
+    /// Menu item
     buildMenuItem(" 1 - Voltar");
     jumpLine(); // Pula linha
     buildMenuItem(" 2 - Pesquisar dados por nome");
     jumpLine(); // Pula linha
 
-    //Linha inferior
+    /// Linha inferior
     buildMenuBottomLine();
 
     printf("\tEscolha uma das opcoes acima: ");
@@ -363,6 +264,7 @@ void dataSearchName() { // Pesquisar dados por nome
 
     switch(change) {
         case 1:
+            clearScreen();
             menu();
             break;
         case 2:
@@ -371,37 +273,42 @@ void dataSearchName() { // Pesquisar dados por nome
             if(search == -1) {
                 clearScreen();
                 printf("\tNenhum registro encontrado.\n");
-                Sleep(2000);
+                Sleep(1500);
                 clearScreen();
             }
 
-            dataSearchName();
+            printf("\tPressione qualquer tecla para continuar...");
+            getch();
 
+            dataSearchName();
             break;
         default:
             clearScreen();
             printf(RED "\tErro no sistema." RESET);
-            Sleep(2000);
+            Sleep(1500);
             clearScreen();
             dataSearchName();
             break;
     }
 }
 
-// 4 --|--
+/// 4 --|--
 void dataSearchLetter() { //Pesquisar dados por letra
     int change;
 
-    //Linha superior
+    /// Limpa tela
+    clearScreen();
+
+    /// Linha superior
     buildMenuTopLine();
 
-    //Menu item
+    /// Menu item
     buildMenuItem(" 1 - Voltar");
     jumpLine(); // Pula linha
     buildMenuItem(" 2 - Pesquisar dados por letra");
     jumpLine(); // Pula linha
 
-    //Linha inferior
+    /// Linha inferior
     buildMenuBottomLine();
 
     printf("\tEscolha uma das opcoes acima: ");
@@ -409,6 +316,7 @@ void dataSearchLetter() { //Pesquisar dados por letra
 
     switch(change) {
         case 1:
+            clearScreen();
             menu();
             break;
         case 2:
@@ -421,6 +329,8 @@ void dataSearchLetter() { //Pesquisar dados por letra
 
             printf("\tProcurar produto pela 1 letra: ");
             scanf(" %c", &product);
+
+            product = toupper(product);
 
             clearScreen();
 
@@ -442,7 +352,7 @@ void dataSearchLetter() { //Pesquisar dados por letra
             if(j == 0) {
                 clearScreen();
                 printf("\tNenhum registro encontrado.\n");
-                Sleep(2000);
+                Sleep(1500);
                 clearScreen();
                 dataSearchLetter();
                 return;
@@ -452,21 +362,24 @@ void dataSearchLetter() { //Pesquisar dados por letra
 
             jumpLine();
 
+            printf("\tPressione qualquer tecla para continuar...");
+            getch();
+
             dataSearchLetter();
 
             break;
         default:
             clearScreen();
             printf(RED "\tErro no sistema." RESET);
-            Sleep(2000);
+            Sleep(1500);
             clearScreen();
             dataSearchLetter();
             break;
     }
 }
 
-// 5 --|--
-void dataEdit() { // Alterar dados
+/// 5 --|--
+void dataEdit() { /// Alterar dados
     int change;
 
     // Limpa tela
@@ -489,6 +402,7 @@ void dataEdit() { // Alterar dados
 
     switch(change) {
         case 1:
+            clearScreen();
             menu();
             break;
         case 2:
@@ -496,6 +410,8 @@ void dataEdit() { // Alterar dados
 
             int n_reg;
             int n_bytes;
+            int validate;
+            int i, j;
 
             FILE *f;
 
@@ -504,7 +420,7 @@ void dataEdit() { // Alterar dados
             if(n_reg == -1) {
                 clearScreen();
                 printf("\tNenhum registro encontrado.\n");
-                Sleep(2000);
+                Sleep(1500);
                 clearScreen();
                 dataEdit();
                 return;
@@ -519,15 +435,21 @@ void dataEdit() { // Alterar dados
             fread(&product1, sizeof(product1), 1, f);
             fread(&amount1, sizeof(amount1), 1, f);
 
-            printf("\tAlterar %s ->: ", product1);
-            scanf("%s", &product1);
+            char aux1[20];
+            int aux2;
+            aux1[20] = product1[20];
+            aux2 = amount1;
 
-            int aux;
-            aux = amount1;
+            for(int i = 0; i < 20; i++) {
+                aux1[i] = product1[i];
+            }
+
+            printf("\tAlterar %s ->: ", aux1);
+            scanf("%s", &product1);
 
             do {
 
-                printf("\tAlterar %d unidade(s) ->: ", aux);
+                printf("\tAlterar %d unidade(s) ->: ", aux2);
                 scanf("%d", &amount1);
 
                 if(amount1 < 0 || amount1 == 10) {
@@ -546,22 +468,22 @@ void dataEdit() { // Alterar dados
 
             clearScreen();
             printf(GRN "\tProduto alterado(a) com sucesso." RESET);
-            Sleep(2500);
+            Sleep(1500);
 
             dataEdit();
             break;
         default:
             clearScreen();
             printf(RED "\tErro no sistema." RESET);
-            Sleep(2000);
+            Sleep(1500);
             clearScreen();
             dataEdit();
             break;
     }
 }
 
-// 6 --|--
-void dataDelete() { // Excluir dados
+/// 6 --|--
+void dataDelete() { /// Excluir dados
     int change;
 
     // Limpa tela
@@ -584,6 +506,7 @@ void dataDelete() { // Excluir dados
 
     switch(change) {
         case 1:
+            clearScreen();
             menu();
             break;
         case 2:
@@ -600,7 +523,7 @@ void dataDelete() { // Excluir dados
             if(n_reg == -1) {
                 clearScreen();
                 printf("\tNenhum registro encontrado.\n");
-                Sleep(2000);
+                Sleep(1500);
                 clearScreen();
                 dataDelete();
                 return;
@@ -627,7 +550,7 @@ void dataDelete() { // Excluir dados
 
             clearScreen();
             printf(GRN "\tProduto excluido(a) com sucesso." RESET);
-            Sleep(2500);
+            Sleep(1500);
 
             dataDelete();
 
@@ -635,15 +558,15 @@ void dataDelete() { // Excluir dados
         default:
             clearScreen();
             printf(RED "\tErro no sistema." RESET);
-            Sleep(2000);
+            Sleep(1500);
             clearScreen();
             dataDelete();
             break;
     }
 }
 
-// 7 --|--
-void quit() { // Fechar o programa
+/// 7 --|--
+void quit() { /// Fechar o programa
 
     for(int i = 3; i >= 0; i--) {
         clearScreen();
@@ -658,9 +581,9 @@ void quit() { // Fechar o programa
 
     exit(0);
 }
-// ----------------------------------------------------------|O*
+/// ----------------------------------------------------------|O*
 
-// Build Menu -----------------------------------------------|X*
+/// Build Menu -----------------------------------------------|X*
 void buildMenuTopLine() { // Linha superior do menu
     //Menu Top --------------------------------------------|
     printf("\t%c", 201);
@@ -696,8 +619,8 @@ void buildMenuItem(char str[]) { // Seleção do menu
     printf("%c", 186);
 }
 
-void buildMenuBottomLine() { // Linha inferior do menu
-    //Menu Bottom -----------------------------------------|
+void buildMenuBottomLine() { /// Linha inferior do menu
+    /// Menu Bottom -----------------------------------------|
     printf("\t%c", 200);
 
     for(int i = 0; i < 36; i++) {
@@ -709,17 +632,17 @@ void buildMenuBottomLine() { // Linha inferior do menu
     jumpLine(); //
 }
 
-void menu() { // Menu
-    //Variavel
-    int change;
-
-    //Limpa tela
+void menu() { /// Menu
+    /// Limpa tela
     clearScreen();
 
-    //Linha superior
+    /// Variavel
+    int change;
+
+    /// Linha superior
     buildMenuTopLine();
 
-    //Menu Item -------------------------------------------|
+    /// Menu Item -------------------------------------------|
     buildMenuItem(" 1 - Entrada de dados");
     jumpLine(); // Pula linha
     buildMenuItem(" 2 - Listar dados");
@@ -735,7 +658,7 @@ void menu() { // Menu
     buildMenuItem(" 7 - Sair");
     jumpLine(); // Pula linha
 
-    //Linha inferior
+    /// Linha inferior
     buildMenuBottomLine();
 
     printf("\tEscolha uma das opcoes acima: ");
@@ -770,15 +693,16 @@ void menu() { // Menu
             clearScreen();
             printf(RED "\tErro no sistema." RESET);
             Sleep(2000);
+            clearScreen();
             menu();
             break;
     }
 }
-// ----------------------------------------------------------|X*
+/// ----------------------------------------------------------|X*
 
-// Home -----------------------------------------------------|O*
+/// Home -----------------------------------------------------|O*
 int main() { // Aplicação
     menu();
     return 0;
 }
-// ----------------------------------------------------------|O*
+/// ----------------------------------------------------------|O*
